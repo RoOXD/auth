ecf1a5c6af73d1d52d005fd4192e27b62a553f76
Cimpean Rares <rares_cimpean@yahoo.com>
2021-07-16 15:47:03 +0300

    sincronizat modificari cu login.php

--- register.php
+++ register.php
@@ -9,8 +9,8 @@ pg_close($dbconn);
 <body>
 <p>Formular inregistrare.Introduceti datele.</p>
 <form action="action.php" method="post">
-Nume utilizator: <input type="text" name="name"><br>
-Parola: <input type="text" name="note"><br>
+Nume utilizator: <input type="text" name="name" required><br>
+Parola: <input type="password" name="note" minlength="6" required><br>
 <input type="submit" name="submit" value="Submit">
 </form>
 
