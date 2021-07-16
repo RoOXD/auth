0b229518d175c3a11f6e99a5b2d1185a53babeaa
Cimpean Rares <rares_cimpean@yahoo.com>
2021-07-16 15:38:52 +0300

    modificat campul parola cu campul corect de tip password, adaugat limita minima de caractere si marcate ambele campuri ca required

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 <body>
 <p>Pagina Login</p>
 <form action="index.php" method="post">
-Numele utilizatorului: <input type="text" name="name"><br>
-Parola: <input type="text" name="note"><br>
+Numele utilizatorului: <input type="text" name="name" required><br>
+Parola: <input type="password" name="note" minlength="6" required><br>
 <input type="submit" name="submit" value="Login">
 </form>
 
