3119aa7f5db416d2d6d28e7950c6f84355f4ffc9
Cimpean Rares <rares_cimpean@yahoo.com>
2021-07-19 15:51:47 +0300

    backup la modificari

--- index.php
+++ index.php
@@ -9,7 +9,11 @@ $hash = $parts[1];
 $val1=hash_hmac('md5',$id, $secret);
 
 if (hash_equals($val1,$hash)){
-	echo "Sunteti logat ca $id.";
+
+	$dbconn = pg_connect("host=localhost port=5432 dbname=auth user=postgres");
+	$query = pg_query($dbconn, "SELECT username FROM users WHERE id='$id'");
+	$arr = pg_fetch_array($query, 0, PGSQL_NUM);
+	echo "Sunteti logat ca $arr[0].";
 ?>
 <html>	
 	<form action="logout.php">
--- login.php
+++ login.php
@@ -9,8 +9,8 @@ if (isset($_COOKIE["UserCookie"])&&!empty($_COOKIE["UserCookie"])){
 <body>
 <p>Pagina Login</p>
 <form action="login.php" method="post">
-Numele utilizatorului: <input type="text" name="name" required><br>
-Parola: <input type="password" name="note" minlength="6" required><br>
+Numele utilizatorului: <input type="text" name="username" required><br>
+Parola: <input type="password" name="password" minlength="6" required><br>
 <input type="submit" name="submit" value="Login">
 </form>
 
@@ -20,37 +20,30 @@ Parola: <input type="password" name="note" minlength="6" required><br>
 if(isset($_POST['submit'])) {
 	$dbconn = pg_connect("host=localhost port=5432 dbname=auth user=postgres");
 
-	$name = $_POST['name'];
+	$username = $_POST['username'];
 
 	$key="14011999";
-	$haz=hash_hmac('md5',$name,$key);
-	$secret="$name.$haz";
-	setcookie("UserCookie", $secret);
 	
 	
-	$note = $_POST['note'];
+	$password = $_POST['password'];
 
-	$query = pg_query($dbconn, "SELECT password FROM users WHERE username='$name'");
-
+	$query = pg_query($dbconn, "SELECT id, password FROM users WHERE username='$username'");
+	
 	$arr = pg_fetch_array($query, 0, PGSQL_NUM);
-	if (password_verify($note, $arr[0])) {
+	if (password_verify($password, $arr[1])) {
+
+		 $hash=hash_hmac('md5',$arr[0],$key);
+        	 $secret="$arr[0].$hash";
+        	 setcookie("UserCookie", $secret);
         	 header('Location: http://127.0.0.1:9000/index.php');
-	?>
-<html>
-        	<form action="logout.php">
-	        <input type="submit" value="Logout" />
-	        </form>
-</html>
-<?php
 	} else {
         	echo 'Parola incorecta.';
 ?>
 <html>
-	        <p>Puteti incerca din nou <a href="http://127.0.0.1:9000/login.php">aici</a> sau va puteti inregistra <a href="http://127.0.0.1:9000/register.php">aici</a></p>
+	        <p>Puteti incerca din nou sau va puteti inregistra <a href="http://127.0.0.1:9000/register.php">aici</a></p>
 </html>
 <?php
 		}
  	}	
 }
 ?>
-
